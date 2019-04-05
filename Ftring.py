41488906304b8b26be3357acd456d9bafaf36976
7eRoM <more7.rev@gmail.com>
2019-04-05 04:40:58 -0700

    Some new methods for clearing of carry-flag added. String generation based on zero-flag completed.

--- Ftring.py
+++ Ftring.py
@@ -5,7 +5,8 @@ from random import randint
 import argparse
 
 
-REGISTERS_TYPE = ["REGISTERS_32", "REGISTERS_16", "REGISTERS_8h", "REGISTERS_8l"]
+REGISTERS_TYPE = ["REGISTERS_32", "REGISTERS_16",
+                  "REGISTERS_8h", "REGISTERS_8l"]
 REGISTERS_32 = ["ebx", "ecx", "edx", "esi", "edi"]
 REGISTERS_16 = ["bx", "cx", "dx", "si", "di"]
 REGISTERS_8h = ["bh", "ch", "dh"]
@@ -13,20 +14,24 @@ REGISTERS_8l = ["bl", "cl", "dl"]
 
 
 def set_flag(flag, method=-1):
+    tmp_set_flag = ""
+
     set_flag_c = [
-    """stc""",
-    """mov $TMP_REG$, $VAL1$
+        """stc""",
+        """mov $TMP_REG$, $VAL1$
     add $TMP_REG$, $VAL2$""",
-    """mov $TMP_REG$, $VAL1$
+        """mov $TMP_REG$, $VAL1$
     sub $TMP_REG$, $VAL2$"""]
+
     if flag == "c":
         if method == -1:
             method = randint(0, len(set_flag_c)-1)
             tmp_set_flag = set_flag_c[method]
-        
+
         if method == 1:
             sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
-            sel_reg = globals()[sel_reg_type][randint(0, len(globals()[sel_reg_type])-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
             tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
             if sel_reg_type == "REGISTERS_32":
                 VAL1 = randint(1, 4294967295)
@@ -43,10 +48,11 @@ def set_flag(flag, method=-1):
 
             tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
             tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
-        
+
         if method == 2:
             sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
-            sel_reg = globals()[sel_reg_type][randint(0, len(globals()[sel_reg_type])-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
             tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
             if sel_reg_type == "REGISTERS_32":
                 VAL1 = randint(1, 4294967295)
@@ -64,31 +70,283 @@ def set_flag(flag, method=-1):
             tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
             tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
 
-        return tmp_set_flag
+    set_flag_z = [
+        """mov $TMP_REG$, $VAL1$
+    add $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    sub $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    inc $TMP_REG$""",
+        """mov $TMP_REG$, $VAL1$
+    dec $TMP_REG$"""]
+
+    if flag == "z":
+        if method == -1:
+            method = randint(0, len(set_flag_z)-1)
+            tmp_set_flag = set_flag_z[method]
+
+        if method == 0:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                VAL2 = 4294967296-VAL1
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                VAL2 = 65536-VAL1
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                VAL2 = 256-VAL1
+            else:
+                VAL1 = randint(0, 255)
+                VAL2 = 256-VAL1
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+            tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 1:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                VAL2 = VAL1
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                VAL2 = VAL1
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                VAL2 = VAL1
+            else:
+                VAL1 = randint(0, 255)
+                VAL2 = VAL1
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+            tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 2:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = 4294967295
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = 65535
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = 255
+            else:
+                VAL1 = 255
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+
+        if method == 3:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            VAL1 = 1
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+
+    return tmp_set_flag
 
 
 def clear_flag(flag, method=-1):
-    clear_flag_c = ["clc"]
+    clear_flag_c = [
+        """clc""",
+        """mov $TMP_REG$, $VAL1$
+    add $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    sub $TMP_REG$, $VAL2$"""]
+
     if flag == "c":
         if method == -1:
             method = randint(0, len(clear_flag_c)-1)
-        return clear_flag_c[method]
+            tmp_clear_flag = clear_flag_c[method]
+
+        if method == 1:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                VAL2 = randint(0, 4294967295-VAL1)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                VAL2 = randint(0, 65535-VAL1)
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255-VAL1)
+            else:
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255-VAL1)
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 2:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                VAL2 = randint(0, VAL1)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                VAL2 = randint(0, VAL1)
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, VAL1)
+            else:
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, VAL1)
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+
+    clear_flag_z = [
+        """mov $TMP_REG$, $VAL1$
+    add $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    sub $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    inc $TMP_REG$""",
+        """mov $TMP_REG$, $VAL1$
+    dec $TMP_REG$"""]
+
+    if flag == "z":
+        if method == -1:
+            method = randint(0, len(clear_flag_z)-1)
+            tmp_clear_flag = clear_flag_z[method]
+
+        if method == 0:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                VAL2 = randint(0, 4294967295)
+                if VAL1 + VAL2 == 4294967296:
+                    VAL1 = (VAL1 + 1) % 4294967296
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                VAL2 = randint(0, 65535)
+                if VAL1 + VAL2 == 65536:
+                    VAL1 = (VAL1 + 1) % 65536
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255)
+                if VAL1 + VAL2 == 256:
+                    VAL1 = (VAL1 + 1) % 256
+            else:
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255)
+                if VAL1 + VAL2 == 256:
+                    VAL1 = (VAL1 + 1) % 256
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 1:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                VAL2 = randint(0, 4294967295)
+                if VAL1 == VAL2:
+                    VAL1 = (VAL1 + 1) % 4294967296
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                VAL2 = randint(0, 65535)
+                if VAL1 == VAL2:
+                    VAL1 = (VAL1 + 1) % 65536
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255)
+                if VAL1 == VAL2:
+                    VAL1 = (VAL1 + 1) % 256
+            else:
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255)
+                if VAL1 == VAL2:
+                    VAL1 = (VAL1 + 1) % 256
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+        
+        if method == 2:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                if VAL1 == 4294967295:
+                    VAL1 = (VAL1 + 1) % 4294967296
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                VAL2 = randint(0, 65535)
+                if VAL1 == VAL2:
+                    VAL1 = (VAL1 + 1) % 65536
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255)
+                if VAL1 == VAL2:
+                    VAL1 = (VAL1 + 1) % 256
+            else:
+                VAL1 = randint(0, 255)
+                VAL2 = randint(0, 255)
+                if VAL1 == VAL2:
+                    VAL1 = (VAL1 + 1) % 256
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+
+        if method == 3:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            VAL1 = randint(0, 4294967295)
+
+            if VAL1 == 1:
+                VAL1 = 2
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+
+    return tmp_clear_flag
 
 
 def mov_flag_to_reg(flag, reg="-1", method=-1):
     mov_flag_to_reg_c = ["setc $REG$"]
+    mov_flag_to_reg_z = ["setz $REG$"]
     if flag == "c":
         if method == -1:
             method = randint(0, len(mov_flag_to_reg_c)-1)
-        return mov_flag_to_reg_c[method].replace("$REG$", reg)
+        tmp_mov_flag_to_reg = mov_flag_to_reg_c[method].replace("$REG$", reg)
+    
+    elif flag == "z":
+        if method == -1:
+            method = randint(0, len(mov_flag_to_reg_z)-1)
+        tmp_mov_flag_to_reg = mov_flag_to_reg_z[method].replace("$REG$", reg)
+
+    return tmp_mov_flag_to_reg
 
 
 def xor_shl(curr_bit, prev_bit, is_lsb, is_msb):
     if curr_bit == "0":
-        res_flag = clear_flag("c")
+        res_flag = clear_flag("z")
     else:
-        res_flag = set_flag("c")
-    res_mov = mov_flag_to_reg("c", reg="bl")
+        res_flag = set_flag("z")
+    res_mov = mov_flag_to_reg("z", reg="bl")
 
     if is_msb:
         bit_res = """
@@ -113,24 +371,25 @@ def bit_generator(curr_bit, prev_bit, is_lsb, is_msb, method=-1):
         res_flag = 756756
     else:
         res_flag = 75676
-    bit_res = globals()[bit_gen_method[method]](curr_bit, prev_bit, is_lsb, is_msb)
+    bit_res = globals()[bit_gen_method[method]](
+        curr_bit, prev_bit, is_lsb, is_msb)
     return bit_res
 
 
 def byte_generator(curr_byte):
     prev_bit = -1
     byte_res = """xor al, al"""
-    for i in range(0,len(curr_byte),1):
+    for i in range(0, len(curr_byte), 1):
         is_lsb = is_msb = False
         if i == 0:
             is_lsb = True
         if i == 7:
-            is_msb = True       
+            is_msb = True
         bit_res = bit_generator(curr_byte[i], prev_bit, is_lsb, is_msb)
         byte_res = byte_res + """
     {0}""".format(bit_res)
         prev_bit = curr_byte[i]
-    
+
     return byte_res
 
 
@@ -138,7 +397,7 @@ def main_func(var, val, type):
     cumm_res = ""
     val = val + "\0"
     char_list = [bin(x)[2:].zfill(8) for x in val.encode('UTF-8')]
-    for i in range(0,len(char_list),1):
+    for i in range(0, len(char_list), 1):
         byte_res = byte_generator(char_list[i])
         if type == "s":
             cumm_res = cumm_res + """
@@ -170,14 +429,15 @@ __asm
     print(res)
 
 
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Flag-Based String Generator, a C++ string obfuscator.')
+    parser = argparse.ArgumentParser(
+        description='Flag-Based String Generator, a C++ string obfuscator.')
     parser.add_argument('--version', action='version', version='Ftring v0.1.0')
     parser.add_argument('-r', '--var', help='varable name', required=True)
-    parser.add_argument('-l', '--val', help='a string would be assigned to the variable', required=True)
-    parser.add_argument('-t', '--type', choices=['s', 'd'], help='type of variable: static array or dynamic array', required=True)
+    parser.add_argument(
+        '-l', '--val', help='a string would be assigned to the variable', required=True)
+    parser.add_argument('-t', '--type', choices=[
+                        's', 'd'], help='type of variable: static array or dynamic array', required=True)
     args = parser.parse_args()
-    
+
     main_func(args.var, args.val, args.type)
