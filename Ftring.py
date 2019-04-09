3b6f319bd4f356dde343f4057b35c86b4f0aa4cd
7eRoM <more7.rev@gmail.com>
2019-04-09 10:24:58 -0700

    O-Flag is not completed but S-Flag is ready now!

--- Ftring.py
+++ Ftring.py
@@ -152,6 +152,202 @@ def set_flag(flag, method=-1):
 
             tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
 
+    set_flag_o = [
+        """mov $TMP_REG$, $VAL1$
+    add $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    sub $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    inc $TMP_REG$""",
+        """mov $TMP_REG$, $VAL1$
+    dec $TMP_REG$"""]
+
+    if flag == "o":
+        if method == -1:
+            method = randint(0, len(set_flag_o)-1)
+            tmp_set_flag = set_flag_o[method]
+
+        if method == 0:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                if VAL1 <= 2147483647:
+                    VAL2 = randint(2147483648-VAL1, 4294967295-VAL1)                  
+                else:
+                    VAL2 = randint(-1*VAL1, 2147483647-VAL1)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                if VAL1 <= 32767:
+                    VAL2 = randint(32768-VAL1, 65535-VAL1)                  
+                else:
+                    VAL2 = randint(-1*VAL1, 32767-VAL1)
+
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(128-VAL1, 255-VAL1)                  
+                else:
+                    VAL2 = randint(-1*VAL1, 127-VAL1)
+            else:
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(128-VAL1, 255-VAL1)                  
+                else:
+                    VAL2 = randint(-1*VAL1, 127-VAL1)
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+            tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 1:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                if VAL1 <= 2147483647:
+                    VAL2 = randint(VAL1+1, VAL1+2147483648)                    
+                else:
+                    VAL2 = randint(VAL1-2147483647, VAL1)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                if VAL1 <= 32767:
+                    VAL2 = randint(VAL1+1, VAL1+32768)                    
+                else:
+                    VAL2 = randint(VAL1-32767, VAL1)
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(VAL1+1, VAL1+128)                    
+                else:
+                    VAL2 = randint(VAL1-127, VAL1)
+            else:
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(VAL1+1, VAL1+128)                    
+                else:
+                    VAL2 = randint(VAL1-127, VAL1)
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+            tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 2:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([2147483647,4294967295])
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([32767,65535])
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([127,255])
+            else:
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([127,255])
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+
+        if method == 3:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([0,2147483648])
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([0,32768])
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([0,128])
+            else:
+                VAL1 = (lambda val_list : val_list[randint(0, 1)])([0,128])
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+
+    set_flag_s = [
+            """mov $TMP_REG$, $VAL1$
+        add $TMP_REG$, $VAL2$""",
+            """mov $TMP_REG$, $VAL1$
+        sub $TMP_REG$, $VAL2$""",
+            """mov $TMP_REG$, $VAL1$
+        inc $TMP_REG$""",
+            """mov $TMP_REG$, $VAL1$
+        dec $TMP_REG$"""]
+
+    if flag == "s":
+        if method == -1:
+            method = randint(0, len(set_flag_o)-1)
+            tmp_set_flag = set_flag_o[method]
+
+        if method == 0:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 2147483647)
+                VAL2 = randint(2147483648-VAL1, 4294967295-VAL1)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 32767)
+                VAL2 = randint(32768-VAL1, 65535-VAL1)
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 127)
+                VAL2 = randint(128-VAL1, 255-VAL1)
+            else:
+                VAL1 = randint(0, 127)
+                VAL2 = randint(128-VAL1, 255-VAL1)
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+            tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 1:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 2147483647)
+                VAL2 = randint(VAL1+1, VAL1+2147483648)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 32767)
+                VAL2 = randint(VAL1+1, VAL1+32768)
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 127)
+                VAL2 = randint(VAL1+1, VAL1+128)
+            else:
+                VAL1 = randint(0, 127)
+                VAL2 = randint(VAL1+1, VAL1+128)
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+            tmp_set_flag = tmp_set_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 2:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = 2147483647
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = 32767
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = 127
+            else:
+                VAL1 = 127
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+
+        if method == 3:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_set_flag = tmp_set_flag.replace("$TMP_REG$", sel_reg)
+            VAL1 = 0
+
+            tmp_set_flag = tmp_set_flag.replace("$VAL1$", str(VAL1))
+
     return tmp_set_flag
 
 
@@ -322,12 +518,237 @@ def clear_flag(flag, method=-1):
 
             tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
 
+    clear_flag_o = [
+        """mov $TMP_REG$, $VAL1$
+    add $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    sub $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    inc $TMP_REG$""",
+        """mov $TMP_REG$, $VAL1$
+    dec $TMP_REG$"""]
+
+    if flag == "o":
+        if method == -1:
+            method = randint(0, len(clear_flag_o)-1)
+            tmp_clear_flag = clear_flag_o[method]
+
+        if method == 0:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                if VAL1 <= 2147483647:
+                    VAL2 = randint(-1*VAL1, 2147483647-VAL1)                                     
+                else:
+                    VAL2 = randint(2147483648-VAL1, 4294967295-VAL1) 
+                    
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                if VAL1 <= 32767:
+                    VAL2 = randint(-1*VAL1, 32767-VAL1)
+                else:
+                    VAL2 = randint(32768-VAL1, 65535-VAL1)
+
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(-1*VAL1, 127-VAL1)
+                else:
+                    VAL2 = randint(128-VAL1, 255-VAL1)                  
+
+            else:
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(-1*VAL1, 127-VAL1)
+                else:
+                    VAL2 = randint(128-VAL1, 255-VAL1)
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 1:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                if VAL1 <= 2147483647:
+                    VAL2 = randint(VAL1-2147483647, VAL1)
+                else:
+                    VAL2 = randint(VAL1+1, VAL1+2147483648) 
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                if VAL1 <= 32767:
+                    VAL2 = randint(VAL1-32767, VAL1)
+                else:
+                    VAL2 = randint(VAL1+1, VAL1+32768)                    
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(VAL1-127, VAL1)
+                else:
+                    VAL2 = randint(VAL1+1, VAL1+128)       
+            else:
+                VAL1 = randint(0, 255)
+                if VAL1 <= 127:
+                    VAL2 = randint(VAL1-127, VAL1)
+                else:
+                    VAL2 = randint(VAL1+1, VAL1+128)
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+        
+        if method == 2:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                if VAL1 == 2147483647 or VAL1 == 4294967295:
+                    VAL1 = VAL1 - 1
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                if VAL1 == 32767 or VAL1 == 65535:
+                    VAL1 = VAL1 - 1
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                if VAL1 == 127 or VAL1 == 255:
+                    VAL1 = VAL1 - 1
+            else:
+                VAL1 = randint(0, 255)
+                if VAL1 == 127 or VAL1 == 255:
+                    VAL1 = VAL1 - 1
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+
+        if method == 3:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(0, 4294967295)
+                if VAL1 == 0 or VAL1 == 2147483648:
+                    VAL1 = VAL1 + 1
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(0, 65535)
+                if VAL1 == 0 or VAL1 == 32768:
+                    VAL1 = VAL1 + 1
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(0, 255)
+                if VAL1 == 0 or VAL1 == 128:
+                    VAL1 = VAL1 + 1
+            else:
+                VAL1 = randint(0, 255)
+                if VAL1 == 0 or VAL1 == 128:
+                    VAL1 = VAL1 + 1
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+
+    clear_flag_s = [
+        """mov $TMP_REG$, $VAL1$
+    add $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    sub $TMP_REG$, $VAL2$""",
+        """mov $TMP_REG$, $VAL1$
+    inc $TMP_REG$""",
+        """mov $TMP_REG$, $VAL1$
+    dec $TMP_REG$"""]
+
+    if flag == "s":
+        if method == -1:
+            method = randint(0, len(clear_flag_s)-1)
+            tmp_clear_flag = clear_flag_s[method]
+
+        if method == 0:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(2147483648, 4294967295)
+                VAL2 = randint(-1*VAL1, 2147483647-VAL1)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(32768, 65535)
+                VAL2 = randint(-1*VAL1, 32767-VAL1)
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(128, 255)
+                VAL2 = randint(-1*VAL1, 127-VAL1)
+            else:
+                VAL1 = randint(128, 255)
+                VAL2 = randint(-1*VAL1, 127-VAL1)               
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+
+        if method == 1:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = randint(2147483648, 4294967295)
+                VAL2 = randint(VAL1-2147483647, VAL1)
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = randint(32768, 65535)
+                VAL2 = randint(VAL1-32767, VAL1)                
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = randint(128, 255)
+                VAL2 = randint(VAL1-127, VAL1)       
+            else:
+                VAL1 = randint(128, 255)
+                VAL2 = randint(VAL1-127, VAL1)
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL2$", str(VAL2))
+        
+        if method == 2:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = 4294967295
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = 65535
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = 255
+            else:
+                VAL1 = 255
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+
+        if method == 3:
+            sel_reg_type = REGISTERS_TYPE[randint(0, len(REGISTERS_TYPE)-1)]
+            sel_reg = globals()[sel_reg_type][randint(
+                0, len(globals()[sel_reg_type])-1)]
+            tmp_clear_flag = tmp_clear_flag.replace("$TMP_REG$", sel_reg)
+
+            if sel_reg_type == "REGISTERS_32":
+                VAL1 = 2147483648
+            elif sel_reg_type == "REGISTERS_16":
+                VAL1 = 32768
+            elif sel_reg_type == "REGISTERS_8h":
+                VAL1 = 128
+            else:
+                VAL1 = 128
+
+            tmp_clear_flag = tmp_clear_flag.replace("$VAL1$", str(VAL1))
+
     return tmp_clear_flag
 
 
 def mov_flag_to_reg(flag, reg="-1", method=-1):
     mov_flag_to_reg_c = ["setc $REG$"]
     mov_flag_to_reg_z = ["setz $REG$"]
+    mov_flag_to_reg_o = ["seto $REG$"]
+    mov_flag_to_reg_s = ["sets $REG$"]
     if flag == "c":
         if method == -1:
             method = randint(0, len(mov_flag_to_reg_c)-1)
@@ -338,15 +759,25 @@ def mov_flag_to_reg(flag, reg="-1", method=-1):
             method = randint(0, len(mov_flag_to_reg_z)-1)
         tmp_mov_flag_to_reg = mov_flag_to_reg_z[method].replace("$REG$", reg)
 
+    elif flag == "o":
+        if method == -1:
+            method = randint(0, len(mov_flag_to_reg_o)-1)
+        tmp_mov_flag_to_reg = mov_flag_to_reg_o[method].replace("$REG$", reg)
+
+    elif flag == "s":
+        if method == -1:
+            method = randint(0, len(mov_flag_to_reg_s)-1)
+        tmp_mov_flag_to_reg = mov_flag_to_reg_s[method].replace("$REG$", reg)
+
     return tmp_mov_flag_to_reg
 
 
 def xor_shl(curr_bit, prev_bit, is_lsb, is_msb):
     if curr_bit == "0":
-        res_flag = clear_flag("z")
+        res_flag = clear_flag("s")
     else:
-        res_flag = set_flag("z")
-    res_mov = mov_flag_to_reg("z", reg="bl")
+        res_flag = set_flag("s")
+    res_mov = mov_flag_to_reg("s", reg="bl")
 
     if is_msb:
         bit_res = """
